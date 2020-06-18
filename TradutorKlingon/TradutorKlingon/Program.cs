ccd148ca023e6771acf3afafaea4a1bf0c6e6d46
Fabio Pereira Barros <fabiop_2@hotmail.com>
2020-06-18 14:07:05 -0300

    inicio do programa

---
+++ TradutorKlingon/TradutorKlingon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TradutorKlingon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Início" + DateTime.Now);
+        }
+    }
+}
